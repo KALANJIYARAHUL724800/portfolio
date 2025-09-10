caf2fb719c3f0effcb8232cdede15ea1eca2678b
smartklrahul <smartklrahul@gmail.com>
2025-09-10 09:29:58 +0530

    again change url

--- src/components/HomeConnection.js
+++ src/components/HomeConnection.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 function getHomeDetails()
 {//http://localhost:8080/api/home https://backend-portfolio-6g6d.onrender.com/api/home 
-  return axios.get("https://backend-portfolio-6g6d.onrender.com")
+  return axios.get("https://backend-portfolio-6g6d.onrender.com/api/home")
     .then(response => {
       return response.data;
     })

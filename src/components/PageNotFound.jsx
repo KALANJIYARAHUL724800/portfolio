3418cde1cb117dcd5322600044bfdab0fca68eaa
smartklrahul <smartklrahul@gmail.com>
2025-09-10 09:40:12 +0530

    add page not found

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -5,9 +5,9 @@ const PageNotFound = () => {
         <div className='container'>
             <br /><br />
             <div className="row">
-                <div className="dflex text-center">
+                <div className="dflex text-center responsive">
                     <div>
-                        <img src="https://media.istockphoto.com/id/1404059706/vector/website-page-not-found-error-404-oops-worried-robot-character-peeking-out-of-outer-space.jpg?s=612x612&w=0&k=20&c=DvPAUof9UsNuNqCJy2Z7ZLLk75qDA3bbLXOOW_50wAk=" />
+                        <img src="https://media.istockphoto.com/id/1404059706/vector/website-page-not-found-error-404-oops-worried-robot-character-peeking-out-of-outer-space.jpg?s=612x612&w=0&k=20&c=DvPAUof9UsNuNqCJy2Z7ZLLk75qDA3bbLXOOW_50wAk=" className='img-fluid'/>
                     </div>
                 </div>
             </div>

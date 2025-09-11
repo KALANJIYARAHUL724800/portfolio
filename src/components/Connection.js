503d2a36c1871ea99fd82a4f6f4fbdcc33b7e5e6
smartklrahul <smartklrahul@gmail.com>
2025-09-11 11:47:27 +0530

    add api

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const CONTACT_URL = "https://backend-portfolio-6g6d.onrender.com/api/contact"; // insert the contact API endpoint
-const ADMIN_URL = "http://localhost:8080/api/admin";//https://backend-portfolio-6g6d.onrender.com/api/admin
+const ADMIN_URL = "https://backend-portfolio-6g6d.onrender.com/api/admin";//https://backend-portfolio-6g6d.onrender.com/api/admin
 //Contact Data send
 const sendContactForm = (val) => {
   return axios.post(`${CONTACT_URL}`, {
